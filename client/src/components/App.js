750bba4b6d58adb53fa3dd0f0664ec2f05c72165
kimkykie <kimkiragu@gmail.com>
2022-02-06 11:32:24 +0300

    update: set up routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import NewStudent from "../pages/NewStudent";
+import Navigation from "./Navigation";
 function App() {
-  return <div className="App">Rendered</div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/new" element={<NewStudent />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
