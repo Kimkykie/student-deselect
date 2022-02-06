46263287cb4b97de18fed5b871337cbec1da10be
kimkykie <kimkiragu@gmail.com>
2022-02-06 14:11:34 +0300

    update: add filter students function

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,58 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
+import Students from "../components/Students";
+import "../styles/home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [students, setStudents] = useState(null);
+  const [nationalities, setNationalities] = useState(null);
+  const [nationalityFilter, setNationalityFilter] = useState(null);
+  const selectInput = useRef(null);
+
+  const filterStudents = () => {
+    const filteredStudents = students.filter(
+      (student) => student.nationality === nationalityFilter
+    );
+
+    return filteredStudents;
+  };
+
+  const handleFilterInput = (event) => {
+    let value = event.target.value;
+    setNationalityFilter(value);
+  };
+
+  useEffect(() => {
+    fetch("/students/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data.students);
+        setNationalities(data.nationalities);
+        setNationalityFilter(data.nationalities[0]);
+      });
+  }, []);
+
+  return (
+    <div className="student-container">
+      {students && nationalities && (
+        <>
+          <select
+            className="student-select"
+            ref={selectInput}
+            onChange={handleFilterInput}
+            defaultValue={nationalities[0]}
+            placeholder="Select a region"
+          >
+            {nationalities.map((nationality) => (
+              <option value={nationality} key={nationality}>
+                {nationality}
+              </option>
+            ))}
+          </select>
+          <Students students={filterStudents()} nationalities={nationalities} />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
