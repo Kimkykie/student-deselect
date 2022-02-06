9facb80f6580ee67e3dc73e19f2ac74d6940b19c
kimkykie <kimkiragu@gmail.com>
2022-02-06 11:38:07 +0300

    feat: add controller to get all students and distinct nationalities

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -3,15 +3,25 @@ const fs = require("fs");
 
 const Student = require("../models/Student");
 
-const students = JSON.parse(
+const seedstudents = JSON.parse(
   fs.readFileSync(__dirname + "/data/students.json", "utf-8")
 );
 
 exports.seedStudents = async (req, res) => {
   try {
     await Student.deleteMany();
-    await Student.insertMany(students);
+    await Student.insertMany(seedstudents);
     res.status(200).send({ message: "Students initialized" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.getStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    const nationalities = await Student.distinct("nationality");
+    res.status(200).send({ students, nationalities });
   } catch (err) {
     res.status(500).send(err);
   }
