b7451d76e99a1a72df193f3bc2e0eedb0010a406
kimkykie <kimkiragu@gmail.com>
2022-02-06 11:00:30 +0300

    feat: add init students into db

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -1,3 +1,18 @@
-exports.seedStudents = (req, res) => {
-  res.json({ status: "success", message: "Students seeded!" });
+const mongoose = require("mongoose");
+const fs = require("fs");
+
+const Student = require("../models/Student");
+
+const students = JSON.parse(
+  fs.readFileSync(__dirname + "/data/students.json", "utf-8")
+);
+
+exports.seedStudents = async (req, res) => {
+  try {
+    await Student.deleteMany();
+    await Student.insertMany(students);
+    res.status(200).send({ message: "Students initialized" });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
